81136898aca6d78219d4ab8e6576a89e20530b53
d_fukuyama <d.fukuyama@gmail.com>
2021-06-03 15:42:55 +0900

    データの読み取りに成功。読み取り内容の検証は未実施。

---
+++ Weight_strain_guage/_strain_guage_build.ps1
@@ -0,0 +1,3 @@
+.\build.ps1;
+.\write.ps1;
+
--- Weight_strain_guage/main.cpp
+++ Weight_strain_guage/main.cpp
@@ -4,11 +4,21 @@
 
 USBSerial  usb(0x1f00, 0x2012, 0x0001, false);
 
+#define DT (int)(LPC_GPIO->W0[19]&1)
+
 int main(void)
 {
 
     LPC_IOCON->PIO0_7 = 0;
-    LPC_GPIO->DIR[0] |= (1<<7);
+    LPC_GPIO->DIR[0] |= (1<<7); // OUTPUT
+
+    LPC_IOCON->PIO0_17 = 0;
+    LPC_GPIO->DIR[0] |= (1<<17); // OUTPUT
+
+    LPC_IOCON->PIO0_19 = 0;
+    LPC_IOCON->PIO0_19 |= (1<<5); // HYS
+    LPC_GPIO->DIR[0] &= ~(1 << 19); // INPUT
+
 
     while(!usb.connected());
     
@@ -18,10 +28,26 @@ int main(void)
     usb.printf("SYSTEM START\r\n");
     usb.printf("-------------------------------------\r\n");
 
+    uint32_t read = 0;
+    int& read_s = *(int*)(&read);  
     for(;;){
-        LPC_GPIO->W0[7] = 0;
-        wait_ms(300);
-        LPC_GPIO->W0[7] = 1;
+        LPC_GPIO->W0[17] = 0;
+        while(DT){ wait_us(1); }
+        wait_us(1);
+        read = 0;
+
+        for(int i=0; i<25; i++){
+            LPC_GPIO->W0[17] = 1;
+            LPC_GPIO->W0[7] = 1;
+            wait_us(3);
+            read = ((read << 1) | DT);
+            LPC_GPIO->W0[17] = 0;
+            LPC_GPIO->W0[7] = 0;
+            wait_us(3);
+        }
+        //read_s ^= (1<<24);
+        //usb.printf("0X_%010X \r\n", read);
+        usb.printf("%d \r\n", read_s);
         wait_ms(100);
     }
     return 0;
