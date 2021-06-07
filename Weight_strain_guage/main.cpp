3e725787b6c7ba46755f90a0edcd3e7ea7bffe0e
３ ふく <d.fukuyama@gmail.com>
2021-06-07 16:12:47 +0900

    Conversionをサブルーチン化

--- Weight_strain_guage/main.cpp
+++ Weight_strain_guage/main.cpp
@@ -6,6 +6,41 @@ USBSerial  usb(0x1f00, 0x2012, 0x0001, false);
 
 #define DT (int)(LPC_GPIO->W0[19]&1)
 
+inline int Convert(int inp) {
+    constexpr int Conversion_Const = (1<<24);
+    return inp^Conversion_Const - Conversion_Const;
+}
+
+enum NextConversion {
+    A_Gain128 = 1,
+    B_Gain64 = 2,
+    A_Gain32 = 3,
+};
+
+
+constexpr int SckReadMax = 24;
+int GetValue(enum NextConversion nc) {
+    uint32_t read = 0;
+    int& read_s = *(int*)(&read);
+
+    LPC_GPIO->W0[17] = 0;
+    while (DT) { wait_us(1); }
+    wait_us(1);
+    read = 0;
+
+    const int imax = SckReadMax + (int)nc;
+    for (int i = 0; i < imax; i++) {
+        LPC_GPIO->W0[17] = 1;
+        LPC_GPIO->W0[7] = 1;
+        wait_us(3);
+        if (i < SckReadMax) read = ((read << 1) | DT);
+        LPC_GPIO->W0[17] = 0;
+        LPC_GPIO->W0[7] = 0;
+        wait_us(3);
+    }
+    return Convert(read_s);
+}
+
 int main(void)
 {
 
@@ -22,31 +57,13 @@ int main(void)
 
     while(!usb.connected());
     
-    wait_ms(1000);
-
     usb.printf("-------------------------------------\r\n");
     usb.printf("SYSTEM START\r\n");
     usb.printf("-------------------------------------\r\n");
 
-    uint32_t read = 0;
-    int& read_s = *(int*)(&read);  
+    int read_s = 0;
     for(;;){
-        LPC_GPIO->W0[17] = 0;
-        while(DT){ wait_us(1); }
-        wait_us(1);
-        read = 0;
-
-        for(int i=0; i<25; i++){
-            LPC_GPIO->W0[17] = 1;
-            LPC_GPIO->W0[7] = 1;
-            wait_us(3);
-            read = ((read << 1) | DT);
-            LPC_GPIO->W0[17] = 0;
-            LPC_GPIO->W0[7] = 0;
-            wait_us(3);
-        }
-        //read_s ^= (1<<24);
-        //usb.printf("0X_%010X \r\n", read);
+        read_s = GetValue(NextConversion::A_Gain128);
         usb.printf("%d \r\n", read_s);
         wait_ms(100);
     }
